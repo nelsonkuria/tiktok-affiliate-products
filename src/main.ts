ccb02b4ad1bb78621b5f798e9247d657910308ea
nelsonkuria <nelsonkuriam@gmail.com>
2026-05-06 05:05:57 +0300

    update input format and process in main

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,25 @@
 import { Actor } from 'apify';
 
+import { findTarget } from './api/helpers.js';
+import { getAffiliateProduct } from './routes/products.js';
 import { seedProducts } from './utils/products.js';
 
 await Actor.init();
 
 interface Input {
-  resource: string;
-  region?: string;
+  endpoint: string;
+  params: Record<string, string | number>;
 }
 
 const input = await Actor.getInput<Input>();
-if (!input) throw new Error('Please provide a resource');
+if (!input) throw new Error('Please provide correct inputs.');
 
-const { resource, region } = input;
-console.log('input', { resource, region });
+const { endpoint, params } = input;
+const target = await findTarget(endpoint);
+
+if (target === '/products') {
+  await getAffiliateProduct(params);
+}
 
 console.log('Seeding products..');
 await seedProducts();
